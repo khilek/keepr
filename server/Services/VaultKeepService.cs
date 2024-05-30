438d724476ea5c5372c77dcbcc2f27a1601e7a30
Khile <khilek@gmail.com>
2024-05-30 14:39:23 -0600

    BACK-END ALL VALID-AUTH, NO-AUTH & INVALID-AUTH POSTMAN TESTS COMPLETED

--- server/Services/VaultKeepService.cs
+++ server/Services/VaultKeepService.cs
@@ -5,18 +5,22 @@ namespace keepr.Services;
 public class VaultKeepService
 {
   private readonly VaultKeepRepository _repository;
-
-  public VaultKeepService(VaultKeepRepository repository)
+  private readonly VaultService _vaultService;
+  public VaultKeepService(VaultKeepRepository repository, VaultService vaultService)
   {
     _repository = repository;
+    _vaultService = vaultService;
   }
   internal VaultKeep CreateVaultKeep(int vaultId, int keepId, Profile userInfo)
   {
-    VaultKeep vaultKeeps = _repository.CreateVaultKeep(vaultId, keepId, userInfo.Id);
-    if (userInfo.Id != vaultKeeps.CreatorId)
+    Vault vault = _vaultService.GetVaultById(vaultId, userInfo.Id);
+    if (vault.CreatorId != userInfo.Id)
     {
       throw new Exception("FAIL");
     }
+
+    VaultKeep vaultKeeps = _repository.CreateVaultKeep(vaultId, keepId, userInfo.Id);
+
     return vaultKeeps;
   }
 
